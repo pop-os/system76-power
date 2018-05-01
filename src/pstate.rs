10785d6bcaa8cf44c9921deadf1516271a7a6a4c
Michael Murphy <mmstickman@gmail.com>
2018-05-01 12:58:10 -0600

    Check if the intel_pstate directory exists

--- src/pstate.rs
+++ src/pstate.rs
@@ -9,12 +9,12 @@ pub struct PState {
 
 impl PState {
     pub fn new() -> io::Result<PState> {
-        //TODO: Check for validity
-        Ok(PState {
-            path: PathBuf::from(
-                "/sys/devices/system/cpu/intel_pstate"
-            )
-        })
+        let path = PathBuf::from("/sys/devices/system/cpu/intel_pstate");
+        if path.is_dir() {
+            Ok(PState { path })
+        } else {
+            Err(io::Error::new(io::ErrorKind::NotFound, "intel_pstate directory not found"))
+        }
     }
 
     pub fn min_perf_pct(&self) -> io::Result<u64> {
@@ -24,7 +24,7 @@ impl PState {
     pub fn set_min_perf_pct(&mut self, value: u64) -> io::Result<()> {
         write_file(self.path.join("min_perf_pct"), format!("{}", value))
     }
-    
+
     pub fn max_perf_pct(&self) -> io::Result<u64> {
         parse_file(self.path.join("max_perf_pct"))
     }
@@ -32,7 +32,7 @@ impl PState {
     pub fn set_max_perf_pct(&mut self, value: u64) -> io::Result<()> {
         write_file(self.path.join("max_perf_pct"), format!("{}", value))
     }
-    
+
     pub fn no_turbo(&self) -> io::Result<bool> {
         let value: u64 = parse_file(self.path.join("no_turbo"))?;
         Ok(value > 0)
@@ -42,4 +42,3 @@ impl PState {
         write_file(self.path.join("no_turbo"), if value { "1" } else { "0" })
     }
 }
-
