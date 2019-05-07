6e56b2b58a376d772bb1691b8e1da040073d0223
Jeremy Soller <jeremy@system76.com>
2019-05-07 13:45:24 -0600

    Update ports for gaze14 hotplug detection

--- src/hotplug.rs
+++ src/hotplug.rs
@@ -5,19 +5,31 @@ use util::read_file;
 pub struct HotPlugDetect {
     sideband: Sideband,
     port: u8,
-    pins: [u8; 3]
+    pins: [u8; 3],
 }
 
 impl HotPlugDetect {
     pub unsafe fn new() -> Result<HotPlugDetect, String> {
         match read_file("/sys/class/dmi/id/product_version").map_err(err_str)?.trim() {
-            "gaze14" |
+            "gaze14" => Ok(HotPlugDetect {
+                sideband: Sideband::new(0xFD00_0000)?,
+                port: 0x6A,
+                pins: [
+                    0x2a, // HDMI
+                    0x00, // Mini DisplayPort (0x2c) is connected to Intel graphics
+                    0x2e, // USB-C
+                ],
+            }),
             "oryp4" |
             "oryp4-b" |
             "oryp5" => Ok(HotPlugDetect {
                 sideband: Sideband::new(0xFD00_0000)?,
                 port: 0x6A,
-                pins: [40, 42, 44],
+                pins: [
+                    0x28, // USB-C
+                    0x2a, // HDMI
+                    0x2c, // Mini DisplayPort
+                ],
             }),
             other => Err(format!("{} does not support hotplug detection", other))
         }
@@ -26,8 +38,11 @@ impl HotPlugDetect {
     pub unsafe fn detect(&self) -> [bool; 3] {
         let mut hpd = [false; 3];
         for i in 0..self.pins.len() {
-            let data = self.sideband.gpio(self.port, self.pins[i]);
-            hpd[i] = data & 2 == 2;
+            let pin = self.pins[i];
+            if pin > 0 {
+                let data = self.sideband.gpio(self.port, pin);
+                hpd[i] = data & 2 == 2;
+            }
         }
         hpd
     }
