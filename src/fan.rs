pop-os/system76-power

commit cc73bfc0500798d2d6b0537200c5a4080b42333b

Update fan curve

src/fan.rs
@@ -171,11 +171,12 @@ impl FanCurve {
     /// The standard fan curve
     pub fn standard() -> Self {
         Self::default()
-            .append(20_00, 30_00)
-            .append(30_00, 35_00)
-            .append(40_00, 42_50)
-            .append(50_00, 52_50)
-            .append(65_00, 10_000)
+            .append(39_99, 0_00)
+            .append(40_00, 40_00)
+            .append(50_00, 50_00)
+            .append(60_00, 65_00)
+            .append(70_00, 85_00)
+            .append(75_00, 100_00)
     }
 
     pub fn get_duty(&self, temp: i16) -> Option<u16> {

commit 754ffc08ebbed46aa09039ba9a20e3f0de686542

Update fan tests
Fixes: cc73bfc05007 ("Update fan curve")

src/fan.rs
@@ -303,16 +303,13 @@ mod tests {
     fn standard_points() {
         let standard = FanCurve::standard();
 
-        assert_eq!(standard.get_duty(0), Some(3000));
-        assert_eq!(standard.get_duty(1000), Some(3000));
-        assert_eq!(standard.get_duty(2000), Some(3000));
-        assert_eq!(standard.get_duty(3000), Some(3500));
-        assert_eq!(standard.get_duty(4000), Some(4250));
-        assert_eq!(standard.get_duty(5000), Some(5250));
-        assert_eq!(standard.get_duty(6000), Some(8417));
-        assert_eq!(standard.get_duty(7000), Some(10000));
-        assert_eq!(standard.get_duty(8000), Some(10000));
-        assert_eq!(standard.get_duty(9000), Some(10000));
+        assert_eq!(standard.get_duty(0), Some(0));
+        assert_eq!(standard.get_duty(3999), Some(0));
+        assert_eq!(standard.get_duty(4000), Some(4000));
+        assert_eq!(standard.get_duty(5000), Some(5000));
+        assert_eq!(standard.get_duty(6000), Some(6500));
+        assert_eq!(standard.get_duty(7000), Some(8500));
+        assert_eq!(standard.get_duty(7500), Some(10000));
         assert_eq!(standard.get_duty(10000), Some(10000));
     }
 }