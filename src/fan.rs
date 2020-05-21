2c88b982c3c0daa416edaeec2780aa88e4bd32d8
Carl Richell <carl@system76.com>
2020-05-21 13:59:05 -0600

    Update fan.rs
    Update XEON fan-curve to improve 205 watt XEON Platinum thermal performance.

--- src/fan.rs
+++ src/fan.rs
@@ -275,10 +275,10 @@ impl FanCurve {
             .append(65_00,  55_00)
             .append(70_00,  60_00)
             .append(75_00,  65_00)
-            .append(78_00,  80_00)
-            .append(81_00,  85_00)
-            .append(83_00,  90_00)
-            .append(85_00, 100_00)
+            .append(77_00,  80_00)
+            .append(79_00,  85_00)
+            .append(81_00,  90_00)
+            .append(83_00, 100_00)
     }
 
     pub fn get_duty(&self, temp: i16) -> Option<u16> {
@@ -400,10 +400,10 @@ mod tests {
         assert_eq!(corex.get_duty(6500), Some(5500));
         assert_eq!(corex.get_duty(7000), Some(6000));
         assert_eq!(corex.get_duty(7500), Some(6500));
-        assert_eq!(corex.get_duty(7800), Some(8000));
-        assert_eq!(corex.get_duty(8100), Some(8500));
-        assert_eq!(corex.get_duty(8300), Some(9000));
-        assert_eq!(corex.get_duty(8500), Some(10000));
+        assert_eq!(corex.get_duty(7700), Some(8000));
+        assert_eq!(corex.get_duty(7900), Some(8500));
+        assert_eq!(corex.get_duty(8100), Some(9000));
+        assert_eq!(corex.get_duty(8300), Some(10000));
         assert_eq!(corex.get_duty(10000), Some(10000));
     }
 }
