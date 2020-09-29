pop-os/system76-power

commit 6c354b745eac684f29c5aedf3a0b144ad35b944b

Add "compute" graphics mode
Introduce a new "compute" mode that performs all rendering on the
integrated graphics, but makes the dGPU available on the bus (with the
NVIDIA drivers loaded) as a compute node.

src/graphics.rs
@@ -10,13 +10,21 @@ use sysfs_class::{PciDevice, SysClass};
 const MODPROBE_PATH: &str = "/etc/modprobe.d/system76-power.conf";
 
 static MODPROBE_NVIDIA: &[u8] = br#"# Automatically generated by system76-power
+options nvidia-drm modeset=1
 "#;
 
 static MODPROBE_HYBRID: &[u8] = br#"# Automatically generated by system76-power
-# http://download.nvidia.com/XFree86/Linux-x86_64/435.17/README/dynamicpowermanagement.html
-options nvidia NVreg_DynamicPowerManagement=0x02
 blacklist i2c_nvidia_gpu
 alias i2c_nvidia_gpu off
+options nvidia NVreg_DynamicPowerManagement=0x02
+options nvidia-drm modeset=1
+"#;
+
+static MODPROBE_COMPUTE: &[u8] = br#"# Automatically generated by system76-power
+blacklist i2c_nvidia_gpu
+alias i2c_nvidia_gpu off
+options nvidia NVreg_DynamicPowerManagement=0x02
+options nvidia-drm modeset=0
 "#;
 
 static MODPROBE_INTEGRATED: &[u8] = br#"# Automatically generated by system76-power
@@ -230,6 +238,8 @@ impl Graphics {
 
                 if mode == "on-demand" {
                     "hybrid".to_string()
+                } else if mode == "off" {
+                    "compute".to_string()
                 } else {
                     "nvidia".to_string()
                 }
@@ -248,6 +258,7 @@ impl Graphics {
         } else if vendor == "nvidia" {
             "on\n"
         } else {
+            // Integrated or Compute
             "off\n"
         };
 
@@ -266,6 +277,8 @@ impl Graphics {
 
             let text = if vendor == "hybrid" {
                 MODPROBE_HYBRID
+            } else if vendor == "compute" {
+                MODPROBE_COMPUTE
             } else if vendor == "nvidia" {
                 MODPROBE_NVIDIA
             } else {
@@ -326,6 +339,8 @@ impl Graphics {
         } else {
             info!("Disabling graphics power");
 
+            // TODO: Don't allow turning off power if nvidia_drm modeset is enabled
+
             unsafe {
                 // Unbind NVIDIA graphics devices and their functions
                 let unbinds = self.nvidia.iter().map(|dev| dev.unbind());
@@ -342,7 +357,7 @@ impl Graphics {
 
     pub fn auto_power(&self) -> Result<(), GraphicsDeviceError> {
         let vendor = self.get_vendor()?;
-        self.set_power(vendor == "nvidia" || vendor == "hybrid")
+        self.set_power(vendor != "integrated")
     }
 
     fn switchable_or_fail(&self) -> Result<(), GraphicsDeviceError> {

src/client.rs
@@ -144,6 +144,7 @@ pub fn client(subcommand: &str, matches: &ArgMatches) -> Result<(), String> {
             _ => profile(&mut client).map_err(err_str),
         },
         "graphics" => match matches.subcommand() {
+            ("compute", _) => client.set_graphics("compute"),
             ("hybrid", _) => client.set_graphics("hybrid"),
             ("integrated", _) | ("intel", _) => client.set_graphics("integrated"),
             ("nvidia", _) => client.set_graphics("nvidia"),

src/main.rs
@@ -59,6 +59,9 @@ fn main() {
                      it is a valid profile",
                 )
                 .subcommand(
+                    SubCommand::with_name("compute")
+                        .about("Like integrated, but the dGPU is available for compute"))
+                .subcommand(
                     SubCommand::with_name("hybrid")
                         .about("Set the graphics mode to Hybrid (PRIME)"),
                 )

commit 4df3ec99e506c6233811177ec5e01d00ccf2bfb2

Disable loading nvidia-drm in compute mode
Prevent the GPU from being used to render displays while in compute
mode, which we see happen with the new OutputSink feature.

Fixes: 6c354b745eac6 ("Add "compute" graphics mode")

src/graphics.rs
@@ -22,9 +22,12 @@ options nvidia-drm modeset=1
 
 static MODPROBE_COMPUTE: &[u8] = br#"# Automatically generated by system76-power
 blacklist i2c_nvidia_gpu
+blacklist nvidia-drm
+blacklist nvidia-modeset
 alias i2c_nvidia_gpu off
+alias nvidia-drm off
+alias nvidia-modeset off
 options nvidia NVreg_DynamicPowerManagement=0x02
-options nvidia-drm modeset=0
 "#;
 
 static MODPROBE_INTEGRATED: &[u8] = br#"# Automatically generated by system76-power