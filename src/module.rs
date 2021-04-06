9712039041083c794f59ecd3cda9162a00bfed70
Daniel Eades <danieleades@hotmail.com>
2021-04-06 15:56:38 +0100

    refactor src/module

--- src/module.rs
+++ src/module.rs
@@ -5,25 +5,17 @@ pub struct Module {
 }
 
 impl Module {
-    fn parse(line: &str) -> io::Result<Module> {
-        let mut parts = line.split(' ');
-
-        let name = parts
-            .next()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "module name not found"))?;
-
-        Ok(Module { name: name.to_string() })
-    }
-
     pub fn all() -> io::Result<Vec<Module>> {
-        let mut modules = Vec::new();
-
-        let data = read_to_string("/proc/modules")?;
-        for line in data.lines() {
-            let module = Module::parse(line)?;
-            modules.push(module);
-        }
-
-        Ok(modules)
+        read_to_string("/proc/modules")?.lines().map(parse).collect()
     }
+}
+
+fn parse(line: &str) -> io::Result<Module> {
+    let name = line
+        .split(' ')
+        .next()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "module name not found"))?
+        .to_string();
+
+    Ok(Module { name })
 }
